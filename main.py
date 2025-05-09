938204ece14aae7258b7ec74d19faae9a2d89536
bradyhernandez <30302763+bradyhernandez@users.noreply.github.com>
2025-05-09 07:32:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -40,9 +40,7 @@ def webhook():
         return jsonify({'message': 'Error'}), 400
 
 
-import os
-
 if __name__ == "__main__":
     from waitress import serve
-    port = int(os.environ.get("PORT", 10000))
-    serve(app, host="0.0.0.0", port=port)
+    serve(app, host="0.0.0.0", port=8080)
+
